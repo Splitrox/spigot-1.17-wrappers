66194aaf2b4bd3c916b13ab5fb468e22d248bc25
Diogo Vaz <49073687+Splitrox@users.noreply.github.com>
2022-01-20 14:56:20 +0000

    Add files via upload

---
+++ EnumItemSlotWrapper.java
@@ -0,0 +1,14 @@
+package com.splitroxdev.theanthill.common.components.wrappers.names;
+
+import net.minecraft.world.entity.EnumItemSlot;
+
+public class EnumItemSlotWrapper {
+
+	public static final EnumItemSlot MAINHAND = EnumItemSlot.a;
+	public static final EnumItemSlot OFFHAND = EnumItemSlot.b;
+	public static final EnumItemSlot FEET = EnumItemSlot.c;
+	public static final EnumItemSlot LEGS = EnumItemSlot.d;
+	public static final EnumItemSlot CHEST = EnumItemSlot.e;
+	public static final EnumItemSlot HEAD = EnumItemSlot.f;
+
+}
